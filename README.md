1d4d97f8a641d379a4a1b151814b0e3286e513df
AshSensei <an9simbol@gmail.com>
2025-05-28 02:04:06 -0700

    service worker part

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Lab8-Starter
+## Lab Participant
+Ashton Simbol (*no other partner*)
+
+## GitHub Pages URL:
+![Github Pages Link](https://ashsensei.github.io/Lab8_CSE110/)
+
+## Graceful Degradation and Service Workers Connection
+In graceful degradation one of the core concepts is that if a component fails the website can still function because you work from the max technology down to the lower levels with "grace." As for the implementation of service workers, even if an aspect such as the internet connection is missing or extremely slow, the service workers can still retrieve the data stored from previous calls and still send data to the web application and prevent a need from having to directly request and fetch data all the time.
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -45,6 +45,17 @@ function initializeServiceWorker() {
   // We first must register our ServiceWorker here before any of the code in
   // sw.js is executed.
   // B1. TODO - Check if 'serviceWorker' is supported in the current browser
+  if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+      navigator.serviceWorker.register("./sw.js")
+      .then (reg=>{
+        console.log("Service worker successfully registered");
+      })
+      .catch(error => {
+        console.error(`Registration failed with ${error}`);
+      });
+  });
+}
   // B2. TODO - Listen for the 'load' event on the window object.
   // Steps B3-B6 will be *inside* the event listener's function created in B2
   // B3. TODO - Register './sw.js' as a service worker (The MDN article
@@ -86,12 +97,12 @@ async function getRecipes() {
   return new Promise((resolve, reject) => {
     // A4. TODO - Loop through each recipe in the RECIPE_URLS array constant
     //            declared above
-    (async ()=>{
+    (async () => {
       try {
-        for (const url of RECIPE_URLS){
+        for (const url of RECIPE_URLS) {
           const res = await fetch(url);
-          if(!res.ok){
-            throw new Error (`Error: ${res.status}`);
+          if (!res.ok) {
+            throw new Error(`Error: ${res.status}`);
           }
           recipes.push(await res.json());
         }
@@ -100,9 +111,9 @@ async function getRecipes() {
           localStorage.setItem("recipes", JSON.stringify(recipes));
           resolve(recipes);
         }
-        
-        
-      } catch (error){
+
+
+      } catch (error) {
         console.error("Error in recipe fetch: ", error);
         reject(error);
       }
